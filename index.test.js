196018db1659aaf230c15ccc7e466fd26bcef1b7
mob-sakai <sakai861104@gmail.com>
2023-08-22 22:47:33 +0900

    build(deps): bump unity-changeset from 2.1.0 to 2.1.1

--- index.test.js
+++ index.test.js
@@ -1,17 +1,17 @@
 const { getUnityChangeset } = require('unity-changeset');
 
 test('get changeset (exist)', async () => {
-  expect(await getUnityChangeset('2021.2.12f1')).toHaveProperty('changeset', '48b1aa000234');
+  await expect(getUnityChangeset('2021.2.12f1')).resolves.toHaveProperty('changeset', '48b1aa000234');
 }, 10000);
 
 test('get changeset (exist, alpha)', async () => {
-  expect(await getUnityChangeset('2022.1.0a9')).toHaveProperty('changeset', '5a1ba9fbc92d');
+  await expect(getUnityChangeset('2022.1.0a9')).resolves.toHaveProperty('changeset', '5a1ba9fbc92d');
 }, 10000);
 
 test('get changeset (exist, beta)', async () => {
-  expect(await getUnityChangeset('2022.1.0b9')).toHaveProperty('changeset', 'a43aced8177f');
+  await expect(getUnityChangeset('2022.1.0b9')).resolves.toHaveProperty('changeset', 'a43aced8177f');
 }, 10000);
 
 test('get changeset (not exist)', async () => {
-  expect(await getUnityChangeset('2021.2.12f2')).toBeUndefined();
+  await expect(getUnityChangeset('2021.2.12f2')).rejects.toThrowError(/^No changeset found for/);
 }, 10000);
