294f7be7c3ca0225c47dc972f76b20d6a10b9a40
mob-sakai <12690315+mob-sakai@users.noreply.github.com>
2024-11-05 05:00:09 +0900

    build(deps): bump unity-changeset from 2.1.1 to 2.2.0

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const { getUnityChangeset } = require('unity-changeset');
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    const db = core.getInput('db');
+    if (!db)
+      throw Error(`input 'db' is empty.`);
+
     const unityVersion = core.getInput('unityVersion');
     if (!unityVersion)
       throw Error(`input 'unityVersion' is empty.`);
 
-    const changeset = await getUnityChangeset(unityVersion);
+    const changeset = await getUnityChangeset(db, unityVersion);
     if (!changeset)
       throw Error(`the version '${unityVersion}' is not found.`);
 
--- index.test.js
+++ index.test.js
@@ -1,17 +1,19 @@
 const { getUnityChangeset } = require('unity-changeset');
 
+const DB_URL = "https://mob-sakai.github.io/unity-changeset/db";
+
 test('get changeset (exist)', async () => {
-  await expect(getUnityChangeset('2021.2.12f1')).resolves.toHaveProperty('changeset', '48b1aa000234');
+  await expect(getUnityChangeset(DB_URL, '2021.2.12f1')).resolves.toHaveProperty('changeset', '48b1aa000234');
 }, 10000);
 
 test('get changeset (exist, alpha)', async () => {
-  await expect(getUnityChangeset('2022.1.0a9')).resolves.toHaveProperty('changeset', '5a1ba9fbc92d');
+  await expect(getUnityChangeset(DB_URL, '2022.1.0a9')).resolves.toHaveProperty('changeset', '5a1ba9fbc92d');
 }, 10000);
 
 test('get changeset (exist, beta)', async () => {
-  await expect(getUnityChangeset('2022.1.0b9')).resolves.toHaveProperty('changeset', 'a43aced8177f');
+  await expect(getUnityChangeset(DB_URL, '2022.1.0b9')).resolves.toHaveProperty('changeset', 'a43aced8177f');
 }, 10000);
 
 test('get changeset (not exist)', async () => {
-  await expect(getUnityChangeset('2021.2.12f2')).rejects.toThrowError(/^No changeset found for/);
+  await expect(getUnityChangeset(DB_URL, '2021.2.12f2')).rejects.toThrowError(/^No changeset found for/);
 }, 10000);
